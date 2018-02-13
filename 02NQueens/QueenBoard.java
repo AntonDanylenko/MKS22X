9f85d808c37b2fbc8b1ad9bb344bbb498e91a8cb
Anton Danylenko <adanylenko@stuy.edu>
2018-02-12 23:11:24 -0500

    working board, but some queens overlap

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -6,26 +6,29 @@ public class QueenBoard{
     }
 
     private boolean addQueen(int r, int c){	
-	/*for(int n=0; n<board[r].length; n++){
-	    board[r][n] += 1;
-	}
-        int p=0;
-	int q=0;
-	while(p<board.length){
-	    board[p][q] +=1;
-	    p++;
-	    q++;
-	}
-	board[r][c] = -1;*/
 	if (board[r][c]!=0){
 	    return false;
 	}
-	for (int n=0; n<board.length; n++){
+	for (int n=c; n<board.length; n++){
 	    board[r][n]+=1;
-	    board[n][c]+=1;
+    }
+	for (int p=r; p<board.length; p++){
+	    board[p][c]+=1;
+    }
+	for (int q=c; q<board.length; q++){
+        try{
+        board[r+q][c+q]+=1;
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+        }
+    }
+	for (int s=c; s<board.length; s++){
+        try{
+        board[r-s][c+s]+=1;
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+        }
 	}
-	//for (int p=0; p<board.length; p++){
-	//}
 	board[r][c] = -1;
 	return true;
     }
@@ -33,6 +36,26 @@ public class QueenBoard{
     private boolean removeQueen(int r, int c){
 	if (board[r][c]!=-1){
 	    return false;
+	}
+	for (int n=c; n<board.length; n++){
+	    board[r][n]-=1;
+    }
+	for (int p=r; p<board.length; p++){
+	    board[p][c]-=1;
+    }
+	for (int q=c; q<board.length; q++){
+        try{
+        board[r+q][c+q]-=1;
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+        }
+    }
+	for (int s=c; s<board.length; s++){
+        try{
+        board[r-s][c+s]-=1;
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+        }
 	}
 	board[r][c] = 0;
 	return true;
@@ -53,25 +76,17 @@ public class QueenBoard{
 	return solveHelp(0);
     }
     
-    public boolean solveHelp(int col){
-	/*for(int p=0; p<board.length; p++){
-	    for (int q=0; q<board.length; q++){
-		if (board[p][q]==0){
-		    addQueen(p,q);
-		    q=0;
-		    p+=1;
-		}
-	    }
-	    }*/
+    private boolean solveHelp(int col){
 	if (col == board.length){
 	    return true;
 	}
 	for (int n=0; n<board.length; n++){
-	    addQueen(n, col);
+	    if(addQueen(n, col)){
 	    if (solveHelp(col+1)){
 		return true;
 	    }
 	    removeQueen(n, col);
+        }
 	}
 	return false;
     }
@@ -81,8 +96,8 @@ public class QueenBoard{
     }
 
     public static void main(String[]args){
-	QueenBoard newBoard = new QueenBoard(8);
-	newBoard.solve();
+	QueenBoard newBoard = new QueenBoard(4);
+	System.out.println(newBoard.solve());
 	System.out.println(newBoard);
     }
 }
