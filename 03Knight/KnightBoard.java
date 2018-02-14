9bd6948d7ce74827d6b0616660cd2f1d2cb7f18a
AntonDanylenko <adanylenko@stuy.edu>
2018-02-14 15:32:01 -0500

    working board, but puts 0 on the last spot of the knight

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -9,28 +9,66 @@ public class KnightBoard{
     }
 
     public String toString(){
+	String result = "";
+	for (int n=0; n<board.length; n++){
+	    for (int r=0; r<board[0].length; r++){
+		result = result + board[r][n] + " ";
+	    }
+	    result += "\n";
+	}
+	return result;
     }
 
     public boolean solve(int startingRow, int startingCol){
-        if (startingRows<0 || startingCols<0){
+        if (startingRow<0 || startingCol<0){
             throw new IllegalArgumentException();
         }
-	return solveHelp(startingRow, startingCol, 0);
+	return solveHelp(startingRow, startingCol, 1);
     }
 
     private boolean solveHelp(int row, int col, int level){
-	if (level == row * col -1){
+	if (row>=board[0].length || col>=board.length ||
+	    row<0 || col<0){
+	    return false;
+	}
+	if (board[row][col]>0){
+	    return false;
+	}
+	/*System.out.println(Text.go(1,1));
+	System.out.println(this);
+	Text.wait(1500); //adjust this delay*/
+	//board[row][col] = level;
+	/*System.out.println(Text.go(1,1));
+	System.out.println(this);
+	Text.wait(1500); //adjust this delay*/
+	if (level == board[0].length * board.length){
 	    return true;
 	}
-	
+	board[row][col] = level;
+	boolean solved =  solveHelp(row+2, col+1, level+1) ||
+	       solveHelp(row+2, col-1, level+1) ||
+	       solveHelp(row-2, col+1, level+1) ||
+	       solveHelp(row-2, col-1, level+1) ||
+	       solveHelp(row+1, col+2, level+1) ||
+	       solveHelp(row+1, col-2, level+1) ||
+	       solveHelp(row-1, col+2, level+1) ||
+	       solveHelp(row-1, col-2, level+1);
+	if (!solved){
+	    board[row][col] = 0;
+        }
+	return solved;
     }
 
     public boolean countSolutions(int startingRow, int startingCol){
-	if (startingRows<0 || startingCols<0){
+	if (startingRow<0 || startingCol<0){
             throw new IllegalArgumentException();
         }
+	return true;
     }
 
     public static void main(String[]args){
+	KnightBoard newBoard = new KnightBoard(7,7);
+	System.out.println(newBoard.solve(0,0));
+	System.out.println(newBoard);
     }
 }
