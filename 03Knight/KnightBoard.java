35d693f3216ef69615aa8053cb1367d2ec499894
AntonDanylenko <adanylenko@stuy.edu>
2018-02-14 14:46:47 -0500

    started other functions

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -7,4 +7,30 @@ public class KnightBoard{
         }
         board = new int[startingRows][startingCols];
     }
+
+    public String toString(){
+    }
+
+    public boolean solve(int startingRow, int startingCol){
+        if (startingRows<0 || startingCols<0){
+            throw new IllegalArgumentException();
+        }
+	return solveHelp(startingRow, startingCol, 0);
+    }
+
+    private boolean solveHelp(int row, int col, int level){
+	if (level == row * col -1){
+	    return true;
+	}
+	
+    }
+
+    public boolean countSolutions(int startingRow, int startingCol){
+	if (startingRows<0 || startingCols<0){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void main(String[]args){
+    }
 }
