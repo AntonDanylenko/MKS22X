86029a09bc01726ef6ae12dc7784ca5ec468e49a
Anton Danylenko <adanylenko@stuy.edu>
2018-03-04 21:51:21 -0500

    animate works too now

--- 04Maze/Driver.java
+++ 04Maze/Driver.java
@@ -1,12 +1,18 @@
+import java.io.*;
 public class Driver{
 
     public static void main(String[]args){
-        Maze f;
-        f = new Maze("data1.dat");//true animates the maze.
+    Maze f;
+	try{
+        f = new Maze("data3.dat");//true animates the maze.
         
         f.setAnimate(true);
         f.solve();
 
         System.out.println(f);
+	}
+	catch(FileNotFoundException e){
+	    System.out.println("There is no file");
+	}
     }
 }
