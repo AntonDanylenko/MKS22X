d7fb23ea17407502a3941a4690e5c843eea961ab
AntonDanylenko <adanylenko@stuy.edu>
2018-03-06 14:45:49 -0500

    started bronze

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,2 +1,20 @@
+import java.util.*;
+import java.io.*;
+
 public class USACO{
+    public static void bronze(String filename){
+        File text = new File(filename);
+        Scanner reader = new Scanner(text);
+	//System.out.println(reader);
+	String file = "";
+	int numRows = 0;
+	int numCols = 0;
+	while (reader.hasNextLine()){
+		String line = reader.nextLine();
+		//System.out.println(line);
+		numCols = line.length();
+		numRows++;
+	    file += line;
+        }
+    }
 }
