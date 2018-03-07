b5bd4abcd63af98d214d1ade931ea96334ce3c09
AntonDanylenko <adanylenko@stuy.edu>
2018-03-07 14:46:20 -0500

    worked on scanning file

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -3,18 +3,24 @@ import java.io.*;
 
 public class USACO{
     public static void bronze(String filename){
-        File text = new File(filename);
-        Scanner reader = new Scanner(text);
-	//System.out.println(reader);
-	String file = "";
-	int numRows = 0;
-	int numCols = 0;
-	while (reader.hasNextLine()){
-		String line = reader.nextLine();
-		//System.out.println(line);
-		numCols = line.length();
-		numRows++;
-	    file += line;
-        }
+	try{
+	    File text = new File(filename);
+	    Scanner reader = new Scanner(text);
+	    int numRows = reader.nextInt();
+	    int numCols = reader.nextInt();
+	    int waterLevel = reader.nextInt();
+	    int numMoves = reader.nextInt();
+	    
+	    System.out.println(numRows + ", " + numCols);
+	    int[][] field = new int[numRows][numCols];
+	    for (int r = 
+	}
+	catch (FileNotFoundException e){
+	    System.out.println("Bad file >:(");
+	}
+    }
+
+    public static void main(String[]args){
+        bronze("makelake.in");
     }
 }
