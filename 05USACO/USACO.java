20a65b9d250edbdb03b3c698574957f2a865d273
AntonDanylenko <adanylenko@stuy.edu>
2018-03-07 15:29:46 -0500

    bronze finished

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class USACO{
-    public static void bronze(String filename){
+    public static int bronze(String filename){
 	try{
 	    File text = new File(filename);
 	    Scanner reader = new Scanner(text);
@@ -10,17 +10,78 @@ public class USACO{
 	    int numCols = reader.nextInt();
 	    int waterLevel = reader.nextInt();
 	    int numMoves = reader.nextInt();
-	    
-	    System.out.println(numRows + ", " + numCols);
 	    int[][] field = new int[numRows][numCols];
-	    for (int r = 
+	    for (int r = 0; r<numRows; r++){
+		for (int c=0; c<numCols; c++){
+		    field[r][c] = reader.nextInt();
+		}
+	    }
+	    /*for (int r = 0; r<numRows; r++){
+		for (int c=0; c<numCols; c++){
+		    System.out.println(field[r][c] + " ");
+		}
+		System.out.println(" ");
+		}*/
+	    //System.out.println(numRows + ", " + numCols);
+
+	    
+	    int[][] moves = new int[numMoves][3];
+	    for (int r = 0; r<numMoves; r++){
+		for (int c=0; c<3; c++){
+		    moves[r][c] = reader.nextInt();
+		}
+	    }
+	    /*for (int r = 0; r<numMoves; r++){
+		for (int c=0; c<3; c++){
+		    System.out.println(moves[r][c] + " ");
+		}
+		System.out.println(" ");
+		}*/
+
+	    
+	    for (int n = 0; n<numMoves; n++){
+		int maxE = 0;
+		for(int r=moves[n][0]-1; r<moves[n][0]+2; r++){
+		    for (int c=moves[n][1]-1; c<moves[n][1]+2; c++){
+			if (field[r][c] > maxE){
+			    maxE = field[r][c];
+			}
+		    }
+		}
+		int cutoff = maxE - moves[n][2];
+		for(int r=moves[n][0]-1; r<moves[n][0]+2; r++){
+		    for (int c=moves[n][1]-1; c<moves[n][1]+2; c++){
+			if (field[r][c] > cutoff){
+			    field[r][c] = cutoff;
+			}
+		    }
+		}
+	    }
+
+
+	    int totalWater = 0;
+	    for (int r=0; r<numRows; r++){
+		for (int c=0; c<numCols; c++){
+		    int volume = waterLevel - field[r][c];
+		    if (volume > 0){
+			field[r][c] = volume;
+		    }
+		    else{
+			field[r][c] = 0;
+		    }
+		    totalWater += field[r][c];
+		}
+	    }
+
+	    return totalWater * 5184;
 	}
 	catch (FileNotFoundException e){
 	    System.out.println("Bad file >:(");
 	}
+	return 0;
     }
 
     public static void main(String[]args){
-        bronze("makelake.in");
+        System.out.println(bronze("makelake.in"));
     }
 }
