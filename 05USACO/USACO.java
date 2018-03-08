eb7da2aa5251d0aefba3228afde4f9ec23b65a1d
AntonDanylenko <adanylenko@stuy.edu>
2018-03-08 15:29:48 -0500

    started silver

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -81,7 +81,63 @@ public class USACO{
 	return 0;
     }
 
+
+
+    public static int silver(String filename){
+	try{
+	    File text = new File(filename);
+	    Scanner reader = new Scanner(text);
+	    int numRows = reader.nextInt();
+	    int numCols = reader.nextInt();
+	    int steps  = reader.nextInt();
+	    reader.nextLine();
+	    char[][] pasture  = new char[numRows][numCols];
+	    for (int r = 0; r<numRows; r++){
+	        pasture[r] = reader.nextLine().toCharArray();
+	    }
+	    /*for (int r=0; r<numRows; r++){
+		for (int c=0; c<numCols; c++){
+		    System.out.println(pasture[r][c] + " ");
+		}
+		System.out.println(" ");
+	    }
+	    System.out.println(numRows + ", " + numCols);*/
+
+	    int R1 = reader.nextInt();
+	    int C1 = reader.nextInt();
+	    int R2 = reader.nextInt();
+	    int C2 = reader.nextInt();
+	    //System.out.println(R1 + ", " + C1 + ", " + R2 + ", " + C2);
+
+	    int[][] board = new int[numRows][numCols];
+	    board[R1][C1] = 1;
+	    for (int n=0; n<=steps; n++){
+		//gotta have two boards
+		for (int r=0; r<numRows; r++){
+		    for (int c=0; c<numCols; c++){
+		        try{
+			    board[r][c] += board[r+1][c];
+			}
+			catch (IndexOutOfBoundsException e){
+			}
+			//repeat all four but onto the second board
+		    }
+		}
+	    }
+
+	    
+	    return 3;
+	}
+	catch (FileNotFoundException e){
+	    System.out.println("Bad file");
+	}
+	return 0;
+    }
+
+    
+
     public static void main(String[]args){
-        System.out.println(bronze("makelake.in"));
+        //System.out.println(bronze("makelake.in"));
+	System.out.println(silver("ctravel.in"));
     }
 }
