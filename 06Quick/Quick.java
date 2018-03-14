0880daf64600d173db6304dd0748796dcde65cb9
AntonDanylenko <adanylenko@stuy.edu>
2018-03-14 15:31:38 -0400

    quicksort works for an array with no repeated values, need to fix to accomodate for this

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -1,5 +1,22 @@
 public class Quick{
+    public static void quicksort(int[] data){
+	quickHelp(data, 0, data.length-1);
+	String line = "";
+        for (int r=0; r<data.length; r++){
+	        line = line + data[r] + ", ";
+        }
+	System.out.println(line);
+    }
 
+    public static void quickHelp(int[] data, int i, int j){
+	int v = partition(data, i, j);
+	if (j>i+1){
+	    quickHelp(data, i, v);
+	    quickHelp(data, v, j);
+	}
+    }
+
+    
     public static int quickselect(int[] data, int k){
 	int j = data.length -1;
 	int i = 0;
@@ -17,12 +34,14 @@ public class Quick{
 	return data[v];
     }
 
+    
     public static void swap(int[] data, int a, int b){
 	int temp = data[b];
 	data[b] = data[a];
 	data[a] = temp;
     }
 
+    
     public static int partition(int[] data, int start, int end){
 	int pivotInd = (int)(Math.random()*(end-start)) + start;
 	int pivotEl = data[pivotInd];
@@ -40,17 +59,17 @@ public class Quick{
 	}
 	swap(data, end, pivotInd);
 	pivotInd = end;
-	String line = "";
+	/*String line = "";
         for (int r=0; r<data.length; r++){
 	        line = line + data[r] + ", ";
         }
-	System.out.println(line);
+	System.out.println(line);*/
 	return pivotInd;
 	}
 
     public static void main(String[]args){
-	//int[] unsorted = {2,5,8,3,1,4,7,6};
-	int[] unsorted = {999,999,999,4,1,0,3,2,999,999,999};
-	System.out.println(quickselect(unsorted, 0));
+	int[] unsorted = {2,5,8,3,1,4,7,6};
+	//int[] unsorted = {999,999,999,4,1,0,3,2,999,999,999};
+        quicksort(unsorted);
     }
 }
