24402c6fe1ef75db518fd05ffb5b38fac0f8c18c
Anton Danylenko <adanylenko@stuy.edu>
2018-03-13 21:04:21 -0400

    started quickselect()

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -1,6 +1,12 @@
-public class Quickselect{
+public class Quick{
 
-    public void sort(){
+    public static int quickselect(int[] data, int k){
+		int result = data.length-1;
+		for(int n=0; n<k; n++){
+			result = partition(data, 0, result);
+			System.out.println(result);
+		}
+		return data[result];
     }
 
     public static void swap(int[] data, int a, int b){
@@ -12,7 +18,6 @@ public class Quickselect{
 	public static int partition(int[] data, int start, int end){
 	int pivotInd = (int)(Math.random()*(end-start)) + start;
 	int pivotEl = data[pivotInd];
-	//System.out.println(pivotInd + ", " + pivotEl);
 	swap(data, pivotInd, start);
 	pivotInd = start;
 	start++;
@@ -24,40 +29,20 @@ public class Quickselect{
 		else{
 			start++;
 		}
-	    /*System.out.println(start + ", " + end);
-	    String line = "";
-	    for (int r=0; r<data.length; r++){
-		line = line + data[r] + ", ";
-	    }
-	    System.out.println(line);*/
 	}
-	/*for (int n=start; n<=end; n++){
-	    while(data[end]>pivotEl){
-		end--;
-	    }
-	    if (data[n]>pivotEl && n<end){
-		swap(data, n, end);
-	    }
-	    System.out.println(n + ", " + end);
-	    String line = "";
-	    for (int r=0; r<data.length; r++){
-		line = line + data[r] + ", ";
-	    }
-	    System.out.println(line);
-	}*/
 	swap(data, end, pivotInd);
 	pivotInd = end;
-	/*String line = "";
+	String line = "";
         for (int r=0; r<data.length; r++){
 	        line = line + data[r] + ", ";
         }
-    System.out.println(line);*/
+    System.out.println(line);
 	return pivotInd;
 	}
 
     public static void main(String[]args){
 	//int[] unsorted = {2,5,8,3,1,4,7,6};
 	int[] unsorted = {999,999,999,4,1,0,3,2,999,999,999};
-	System.out.println(partition(unsorted, 0, unsorted.length -1));
+	System.out.println(quickselect(unsorted, 2));
     }
 }
