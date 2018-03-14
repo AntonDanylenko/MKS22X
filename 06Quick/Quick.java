959f049e5cd91a9dafc5471879446b1ef14a691d
AntonDanylenko <adanylenko@stuy.edu>
2018-03-14 15:06:15 -0400

    quickselect finished

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -1,12 +1,20 @@
 public class Quick{
 
     public static int quickselect(int[] data, int k){
-		int result = data.length-1;
-		for(int n=0; n<k; n++){
-			result = partition(data, 0, result);
-			System.out.println(result);
-		}
-		return data[result];
+	int j = data.length -1;
+	int i = 0;
+	int v = partition(data, i, j);
+	while(v!=k){
+	    if (v>k){
+		j = v;
+	    }
+	    else if(v<k){
+		i = v;
+	    }
+	    v = partition(data, i, j);
+	    System.out.println(v);
+	}
+	return data[v];
     }
 
     public static void swap(int[] data, int a, int b){
@@ -15,7 +23,7 @@ public class Quick{
 	data[a] = temp;
     }
 
-	public static int partition(int[] data, int start, int end){
+    public static int partition(int[] data, int start, int end){
 	int pivotInd = (int)(Math.random()*(end-start)) + start;
 	int pivotEl = data[pivotInd];
 	swap(data, pivotInd, start);
@@ -36,13 +44,13 @@ public class Quick{
         for (int r=0; r<data.length; r++){
 	        line = line + data[r] + ", ";
         }
-    System.out.println(line);
+	System.out.println(line);
 	return pivotInd;
 	}
 
     public static void main(String[]args){
 	//int[] unsorted = {2,5,8,3,1,4,7,6};
 	int[] unsorted = {999,999,999,4,1,0,3,2,999,999,999};
-	System.out.println(quickselect(unsorted, 2));
+	System.out.println(quickselect(unsorted, 0));
     }
 }
