745f377caf24788a1cd075583e64d58f8b1cc9bc
AntonDanylenko <adanylenko@stuy.edu>
2018-03-15 14:52:05 -0400

    dutch flag partition workks now

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -67,11 +67,43 @@ public class Quick{
         }
 	System.out.println(line);*/
 	return pivotInd;
-	}
+    }
 
+    public static int[] part(int[] data, int start, int end){
+	int i = (int)(Math.random()*(end-start)) + start;
+	int pivot = data[i];
+	System.out.println(pivot);
+	swap(data, i, start);
+	i = start+1;
+	while (i<end){
+	    if (data[i]==pivot){
+		i++;
+	    }
+	    else if (data[i]>pivot){
+		swap(data,i,end);
+		end--;
+	    }
+	    else{
+		swap(data,i,start);
+		i++;
+		start++;
+	    }
+	}
+	int[] array = new int[2];
+	array[0] = start;
+	array[1] = i;
+	String line = "";
+        for (int r=0; r<data.length; r++){
+	        line = line + data[r] + ", ";
+        }
+	System.out.println(line);
+	return array;
+    }
     public static void main(String[]args){
 	//int[] unsorted = {2,5,8,3,1,4,7,6};
-	int[] unsorted = {999,999,999,4,1,0,3,2,999,999,999};
-        quicksort(unsorted);
+	//int[] unsorted = {999,999,999,4,1,0,3,2,999,999,999};
+	int[] unsorted = {1,2,0,1,1,2,1,1,0,0,0,2,0,1,0,0,2,1,0,2,0,1,0,2,0,2};
+	//int[] unsorted = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        part(unsorted, 0, unsorted.length-1);
     }
 }
