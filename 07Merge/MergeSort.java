7a14b1e690d652e1ab87f3aa486caae6b607e0d1
AntonDanylenko <adanylenko@stuy.edu>
2018-03-19 14:36:58 -0400

    problem with array length of mergesort()

--- 07Merge/MergeSort.java
+++ 07Merge/MergeSort.java
@@ -1,5 +1,25 @@
 public class MergeSort{
-    public static void mergesort(int[] data, int start, int end){
+    public static int[] mergesort(int[] data, int start, int end){
+	if(start==end){
+	    return data;
+	}
+	
+	int[] data1 = mergesort(data, start, (start+end)/2);
+	String line = "";
+        for (int r=0; r<data1.length; r++){
+	        line = line + data1[r] + ", ";
+        }
+	System.out.println(line);
+	
+	int[] data2 = mergesort(data, (start+end)/2 +1, end);
+	String line1 = "";
+        for (int r=0; r<data2.length; r++){
+	        line1 = line1 + data2[r] + ", ";
+        }
+	System.out.println(line1);
+	
+	int[] result = merge(data1, data2);
+	return result;
     }
 
     public static int[] merge(int[] data1, int[] data2){
@@ -33,9 +53,13 @@ public class MergeSort{
 	//int[] data1 = {2,4,7,9,10};
 	//int[] data2 = {1,3,5,6,8};
 
-	int[] data1 = {5,7,8,8,9,10};
-	int[] data2 = {1,2,3,4,4,5,6};
-	int[] data = merge(data1, data2);
+	//int[] data1 = {5,7,8,8,9,10};
+	//int[] data2 = {1,2,3,4,4,5,6};
+	
+	//int[] data = merge(data1, data2);
+	
+	int[] data0 = {2,5,1,3,9,10,4,6,7,8};
+	int[] data = mergesort(data0,0,data0.length-1);
 	String line = "";
         for (int r=0; r<data.length; r++){
 	        line = line + data[r] + ", ";
