2eddb07e58697653643636b414a814aa65b7a4b0
AntonDanylenko <adanylenko@stuy.edu>
2018-03-23 14:39:22 -0400

    made quicksort as quick as it has to be

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -1,33 +1,24 @@
 public class Quick{
     public static void quicksort(int[] data){
-	quickHelp2(data, 0, data.length-1);
-	String line = "";
+	quickHelp(data, 0, data.length-1);
+	/*String line = "";
         for (int r=0; r<data.length; r++){
 	        line = line + data[r] + ", ";
         }
-	//System.out.println(line);
+	System.out.println(line);*/
     }
 
-    /*public static void quickHelp(int[] data, int i, int j){
-	int v = partition(data, i, j);
-	if (i!=v){
-	    quickHelp(data, i, v-1);
-	}
-	if (j!=v){
-	    quickHelp(data, v+1, j);
-	}
-	}*/
 
-    public static void quickHelp2(int[] data, int i, int j){
+    public static void quickHelp(int[] data, int i, int j){
 	int[] v = part(data, i, j);
 	//System.out.println("i: " + i + ",j: " + j);
 	if (i<v[0]){
 	    //System.out.println("i: " + i + ",j: " + (v[0]-1));
-	    quickHelp2(data, i, v[0]-1);
+	    quickHelp(data, i, v[0]-1);
 	}
 	if (j>v[1]){
 	    //System.out.println("i: " + (v[1]+1) + ",j: " + j);
-	    quickHelp2(data, v[1]+1, j);
+	    quickHelp(data, v[1]+1, j);
 	}
     }
     
@@ -38,11 +29,11 @@ public class Quick{
 	int j = data.length -1;
 	int i = 0;
 	int[] v = part(data, i, j);
-	String line = "";
+	/*String line = "";
         for (int r=0; r<data.length; r++){
 	        line = line + data[r] + ", ";
         }
-	//System.out.println(line);
+	System.out.println(line);*/
 	while(!(v[0]<=k && v[1]>=k)){
 	    if (v[0]>k){
 		j = v[0];
@@ -67,32 +58,7 @@ public class Quick{
 	data[b] = data[a];
 	data[a] = temp;
     }
-
     
-    public static int partition(int[] data, int start, int end){
-	int pivotInd = (int)(Math.random()*(end-start)) + start;
-	int pivotEl = data[pivotInd];
-	swap(data, pivotInd, start);
-	pivotInd = start;
-	start++;
-	while(start<=end){
-		if (data[start]>=pivotEl){
-			swap(data, start, end);
-			end--;
-		}
-		else{
-			start++;
-		}
-	}
-	swap(data, end, pivotInd);
-	pivotInd = end;
-	/*String line = "";
-        for (int r=0; r<data.length; r++){
-	        line = line + data[r] + ", ";
-        }
-	System.out.println(line);*/
-	return pivotInd;
-    }
 
     public static int[] part(int[] data, int start, int end){
 	int i = (int)(Math.random()*(end-start)) + start;
@@ -101,23 +67,21 @@ public class Quick{
 	swap(data, i, start);
 	i = start+1;
 	while (i<=end){
-	    if (data[i]==pivot){
+	    if (data[i]<pivot){
+		swap(data,i,start);
 		i++;
+		start++;
 	    }
 	    else if (data[i]>pivot){
 		swap(data,i,end);
 		end--;
 	    }
 	    else{
-		swap(data,i,start);
 		i++;
-		start++;
 	    }
 	}
-	int[] array = new int[2];
-	array[0] = start;
+	int[] array = {start, end};
 	//System.out.println("start: " + start + ", end: " + end);
-	array[1] = end;
 	/*String line = "";
         for (int r=0; r<data.length; r++){
 	        line = line + data[r] + ", ";
