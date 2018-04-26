19135c18d1a3849aabcefc9fd711aaed1dbada16
Anton Danylenko <adanylenko@stuy.edu>
2018-04-25 22:51:36 -0400

    confused about some parts of addFirst and addLast

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -3,13 +3,19 @@ import java.util.LinkedList;
 
 public class MyDeque<E>{
 	private E[] deque;
+	private int front;
+	private int back;
 	
 	@SuppressWarnings("unchecked") public MyDeque(){
 		deque = (E[]) new Object[10];
+		front = 0;
+		back = 0;
 	}
 	
 	@SuppressWarnings("unchecked") public MyDeque(int capacity){
 		deque = (E[]) new Object[capacity];
+		front = 0;
+		back = 0;
 	}
 	
 	public String toString(){
@@ -20,10 +26,79 @@ public class MyDeque<E>{
 		return deque.length;
 	}
 	
+	@SuppressWarnings("unchecked") private void doubleCap(){
+		E[] temp = (E[]) new Object[size() * 2];
+		if (back<front){
+			for (int n=0; n<=back; n++){
+				temp[n] = deque[n];
+			}
+			for (int r=front; r<size(); r++){
+				temp[temp.length - size() + r] = deque[r];
+			}
+		}
+		else{
+			for (int n=front; n<=back; n++){
+				temp[n] = deque[n];
+			}
+		}
+		deque = temp;
+	}
+
+	public void addFirst(E element){
+		if(element == null){
+			throw new NullPointerException();
+		}
+		if (deque[front].equals(null)){
+			deque[front] = element;
+		}
+		else{
+			if (front<1){
+				if (!deque[size()-1].equals(null)){
+					doubleCap();
+				}
+				deque[size()-1] = element;
+				front = size()-1;
+			}
+			else{
+				if (!deque[front-1].equals(null)){
+					doubleCap();
+				}
+				deque[front-1] = element;
+				front--;
+			}
+		}
+	}
+
+	public void addLast(E element){
+		if(element == null){
+			throw new NullPointerException();
+		}
+		if (deque[back].equals(null)){
+			deque[back] = element;
+		}
+		else{
+			if (back+1==size()){
+				if (!deque[0].equals(null)){
+					doubleCap();
+				}
+				deque[0] = element;
+				back = 0;
+			}
+			else{
+				if (!deque[back + 1].equals(null)){
+					doubleCap();
+				}
+				deque[back + 1] = element;
+				back++;
+			}
+		}
+	}
 	
 	public static void main(String[]args){
-		MyDeque thing = new MyDeque();
+		MyDeque<Integer> thing = new MyDeque<Integer>();
 		System.out.println(thing);
 		System.out.println(thing.size());
+		thing.addFirst(7);
+		System.out.println(thing);
 	}
 }
