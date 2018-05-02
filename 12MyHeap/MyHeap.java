9eea226552c457ce68d129568623e765ddde031e
AntonDanylenko <adanylenko@stuy.edu>
2018-05-02 15:30:50 -0400

    constructors

--- 12MyHeap/MyHeap.java
+++ 12MyHeap/MyHeap.java
@@ -1,8 +1,16 @@
 public class MyHeap{
+    private String[] data;
+    private boolean max;
+    private int size;
+    
     public MyHeap(){
+	max = true;
+	data = new String[10];
     }//- construct empty max heap
 
     public MyHeap(boolean bool){
+	max = bool;
+	data = new String[10];
     } //- true: construct empty max heap, false: construct empty min heap.
 
     public void add(String s){
