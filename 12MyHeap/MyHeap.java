01872a1f84eb52385d16805332e1f1241505c9be
AntonDanylenko <adanylenko@stuy.edu>
2018-05-02 15:21:08 -0400

    start

---
+++ 12MyHeap/MyHeap.java
@@ -0,0 +1,23 @@
+public class MyHeap{
+    public MyHeap(){
+    }//- construct empty max heap
+
+    public MyHeap(boolean bool){
+    } //- true: construct empty max heap, false: construct empty min heap.
+
+    public void add(String s){
+	
+    }
+    
+    public String remove(){
+	return "";
+    }
+
+    public String peek(){
+	return "";
+    }
+
+    public int size(){
+	return -1;
+    }
+}
