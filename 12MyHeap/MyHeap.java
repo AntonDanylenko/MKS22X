4fc0ad4faa67dad2e22f5f3868ab8cf232370909
AntonDanylenko <adanylenko@stuy.edu>
2018-05-10 14:44:51 -0400

    reverted

--- 12MyHeap/MyHeap.java
+++ 12MyHeap/MyHeap.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class MyHeap{
     private String[] data;
     private boolean max;
@@ -15,89 +13,10 @@ public class MyHeap{
 	data = new String[10];
     } //- true: construct empty max heap, false: construct empty min heap.
 
-	public String toString(){
-		System.out.println(Arrays.toString(data));
-		String result = "";
-		int index = 0;
-		int rows = (int) (Math.log(size) / Math.log(2));
-		for (int s=0; s<=rows; s++){
-			for (int n=0; n<Math.pow(2, rows-s)-1; n++){
-				result += " ";
-			}
-			result += data[index];
-			index++;
-			for (int r=0; r<Math.pow(2,s)-1 && index<size; r++){
-				for (int n=0; n<Math.pow(2, rows-s+1)-1; n++){
-					result += " ";
-				}
-				result += data[index];
-				index++;
-			}
-			result += "\n";
-		}
-		
-		
-		/*for (int n=0; n<Math.pow(2, rows)-1; n++){
-			result += " ";
-		}
-		result = result + data[0] + "\n";
-		
-		for (int n=0; n<Math.pow(2, rows-1)-1; n++){
-			result += " ";
-		}
-		result = result + data[1];
-		for (int n=0; n<Math.pow(2, rows)-1; n++){
-			result += " ";
-		}
-		result = result + data[2] + "\n";*/
-		
-		
-		return result;
-	}
-	
     public void add(String s){
-		if (size>=data.length){
-			resize();
-		}
-		data[size]=s;
-		size++;
-		int n=size-1;
-		while(n>=0){
-			if (max){
-				if (data[n].compareTo(data[(n-1)/2]) >0){
-					swap(n, (n-1)/2);
-					n=(n-1)/2;
-				}
-				else{
-					n=-1;
-				}
-			}
-			else{
-				if (data[n].compareTo(data[(n-1)/2]) <0){
-					swap(n,(n-1)/2);
-					n=(n-1)/2;
-				}
-				else{
-					n=-1;
-				}
-			}
-		}
+	
     }
-	
-	public void resize(){
-		String[] temp = new String[data.length*2];
-		for (int n=0; n<size; n++){
-			temp[n] = data[n];
-		}
-		data = temp;
-	}
     
-	public void swap(int a, int b){
-		String temp = data[a];
-		data[a] = data[b];
-		data[b] = temp;
-	}
-	
     public String remove(){
 	return "";
     }
@@ -109,19 +28,4 @@ public class MyHeap{
     public int size(){
 	return -1;
     }
-	
-	public static void main(String[]args){
-		MyHeap heap = new MyHeap();
-		heap.add("a");
-		System.out.println(heap);
-		heap.add("d");
-		System.out.println(heap);
-		heap.add("b");
-		heap.add("c");
-		heap.add("e");
-		heap.add("g");
-		heap.add("f");
-		heap.add("a");
-		System.out.println(heap);
-	}
 }
