6425ba293ef6e0116ca709e52f158563f5a78559
AntonDanylenko <adanylenko@stuy.edu>
2018-05-10 14:22:50 -0400

    b

--- 14MazeTwo/Location.java
+++ 14MazeTwo/Location.java
@@ -1,3 +1,22 @@
-public class FrontierQueue implements Frontier{
-    private Queue queue;
+public class Location{
+    private int x,y;
+    private Location previous;
+
+    public Location(int _x, int _y, Location prev){
+	x=_x;
+	y=_y;
+	previous=prev;
+    }
+
+    public int getX(){
+	return x;
+    }
+
+    public int getY(){
+	return y;
+    }
+
+    public Location getPrevious(){
+	return previous;
+    }
 }
