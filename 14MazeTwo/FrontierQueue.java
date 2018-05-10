65a61664ee88978c723b07b289a45dc7b8f17aa1
AntonDanylenko <adanylenko@stuy.edu>
2018-05-10 14:50:26 -0400

    reverted to the right FrontierQueue

--- 14MazeTwo/FrontierQueue.java
+++ 14MazeTwo/FrontierQueue.java
@@ -1,3 +1,37 @@
 public class FrontierQueue implements Frontier{
-    private Deque deque;
+    private String[] queue;
+	private int size;
+	
+	public FrontierQueue(){
+		queue = new String[10];
+	}
+	
+	public void resize(){
+		String[] temp = new String[queue.length*2];
+		for (int n=0; n<size; n++){
+			temp[n] = queue[n];
+		}
+		queue = temp;
+	}
+
+	public Location next(){
+		Location result = queue[0];
+		for (int n=1; n<size; n++){
+			queue[n-1] = queue[n];
+		}
+		size--;
+		return result;
+	}
+
+	public void add(Location n){
+		if (size==queue.length){
+			resize();
+		}
+		queue[size] = n;
+		size++;
+	}
+
+	public boolean hasNext(){
+		return size!=0;
+	}
 }
--- 14MazeTwo/FrontierStack.java
+++ 14MazeTwo/FrontierStack.java
@@ -1,3 +1,35 @@
 public class FrontierStack implements Frontier{
-    private Stack deque;
+    private String[] stack;
+	private int size;
+	
+	public FrontierStack(){
+		stack = new String[10];
+	}
+	
+	public void resize(){
+		String[] temp = new String[stack.length*2];
+		for (int n=0; n<size; n++){
+			temp[n] = stack[n];
+		}
+		stack = temp;
+	}
+
+	public Location next(){
+		Location result = stack[size-1];
+		stack[size-1] = null;
+		size--;
+		return result;
+	}
+
+	public void add(Location n){
+		if (size==stack.length){
+			resize();
+		}
+		stack[size] = n;
+		size++;
+	}
+
+	public boolean hasNext(){
+		return size!=0;
+	}
 }
