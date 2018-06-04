e1df74ddc88542fd352a94e5a2b03bbb3f6ff9f0
Anton Danylenko <adanylenko@stuy.edu>
2018-06-03 21:18:10 -0400

    fixed start and end

--- 14MazeTwo/Maze.java
+++ 14MazeTwo/Maze.java
@@ -4,32 +4,53 @@ import java.io.*;
 public class Maze{
   private Location start,end;
   private char[][] board;
-  private int maxrows, maxcols;
 
-  public Maze(String mazeText){
-	maxcols = mazeText.indexOf('\n');
-	for (int n=0; n<mazeText.length(); n++){
-		if (mazeText.charAt(n) == '\n'){
-			maxrows++;
+  public Maze(String boardText){
+	ArrayList<char[]> lines = new ArrayList<char[]>();
+	int startRow=-1, startCol=-1;
+	int endRow=-1,endCol=-1;
+	try{
+	    Scanner file = new Scanner(new File(boardText));
+	    while(file.hasNext()){
+		lines.add(file.nextLine().toCharArray());
+	    }
+	}catch(FileNotFoundException e){
+	    System.out.println("File not found: "+boardText);
+	    System.exit(1);
+	}
+	board = new char[lines.size()][];
+	for(int n = 0; n < board.length; n++){
+	    board[n]=lines.get(n);
+	}
+	for(int r=0; r<board.length;r++){
+	    for(int c=0; c<board[r].length;c++){
+		if(board[r][c]=='S'){
+		    if(startRow == -1){
+			startRow=r;
+			startCol=c;
+		    }else{
+			System.out.println("More than one 'S'");
+			System.exit(0);
+		    }
 		}
-	}
-	System.out.println("Cols: " + maxcols + ", Rows: " + maxrows);
-	board = new char[maxrows][maxcols];
-	for (int r=0; r<maxrows; r++){
-		for (int c=0; c<maxcols; c++){
-			char temp = mazeText.charAt(r*(maxcols+1)+c);
-			if (temp=='S'){
-				start = new Location(c,r, null);
-			}
-			else if(temp=='E'){
-				end = new Location(c,r, null);
-			}
-			board[r][c] = temp;
+
+		if(board[r][c]=='E'){
+		    if(endRow == -1){
+			endRow=r;
+			endCol=c;
+		    }else{
+			System.out.println("More than one 'E'");
+			System.exit(0);
+		    }
 		}
+	    }
 	}
-	for (int n=0; n<board.length; n++){
-		System.out.println(Arrays.toString(board[n]));
+	if(startRow == -1 || endRow == -1){
+	    System.out.println("No 'S' or 'E' found");
+	    System.exit(0);
 	}
+	end = new Location(endRow,endCol,null);
+	start = new Location(startRow,startCol,null);
   }
 
   // '#' - wall 
@@ -85,16 +106,8 @@ public class Maze{
   }
 
   public static void main(String[]args) throws FileNotFoundException{
-	File text = new File("data1.dat");
-    Scanner reader = new Scanner(text);
-	String file = "";
-	while (reader.hasNextLine()){
-		String line = reader.nextLine();
-	    file += line;
-		file += "\n";
-	}
-	System.out.println(file);
-	Maze test = new Maze(file);
+	Maze test = new Maze("data1.dat");
+	System.out.println(test);
   }
 }
 
