373d711c29a538bd6a41c3b2a80112366bf4ab27
Anton Danylenko <adanylenko@stuy.edu>
2018-06-16 11:35:05 -0400

    think i fixed errors

--- 12MyHeap/MyHeap.java
+++ 12MyHeap/MyHeap.java
@@ -26,7 +26,7 @@ public class MyHeap<T extends Comparable<T>>{
 			for (int n=0; n<Math.pow(2, rows-s)-1; n++){
 				result += " ";
 			}
-			result += data[index];
+			result += data[index]; 
 			index++;
 			for (int r=0; r<Math.pow(2,s)-1 && index<size; r++){
 				for (int n=0; n<Math.pow(2, rows-s+1)-1; n++){
@@ -46,29 +46,20 @@ public class MyHeap<T extends Comparable<T>>{
 		}
 		data[size]=s;
 		size++;
-		int n=size-1;
-		while(n>=0){
-			if (max){
-				if (data[n].compareTo(data[(n-1)/2]) >0){
-					swap(n, (n-1)/2);
-					n=(n-1)/2;
-				}
-				else{
-					n=-1;
-				}
-			}
-			else{
-				if (data[n].compareTo(data[(n-1)/2]) <0){
-					swap(n,(n-1)/2);
-					n=(n-1)/2;
-				}
-				else{
-					n=-1;
-				}
-			}
-		}
+		pushup(size-1);
     }
 	
+	public void pushup(int n){
+		if (max && data[n].compareTo(data[(n-1)/2]) >0){
+			swap(n,(n-1)/2);
+			pushup((n-1)/2);
+		}
+		else if (!max && data[n].compareTo(data[(n-1)/2]) <0){
+			swap(n,(n-1)/2);
+			pushup((n-1)/2);
+		}
+	}
+
 	@SuppressWarnings("unchecked")
 	public void resize(){
 		T[] temp = (T[]) new Comparable[data.length*2];
@@ -89,24 +80,45 @@ public class MyHeap<T extends Comparable<T>>{
 	data[0] = data[size-1]; //SWITCH THEN PUSH UP
 	data[size-1] = null;
 	size--;
-	int index = 0;
-	int rows = (int) (Math.log(size) / Math.log(2));
-	while (2*index+2<size){
-	    if (data[2*index+1].compareTo(data[2*index+2])<0 ^ max){
-	        swap(index, 2*index+1);
-	        index = 2*index+1;
-	    }
-	    else{
-	        swap(index, 2*index+2);
-			index = 2*index+2;
-	    }
-	}
-	if (2*index+2==size && (data[index].compareTo(data[2*index+1])>0 ^ max)){
-		swap(index, 2*index+1);
-	}
+	pushdown(0);
 	return element;
     }
 	
+	public void pushdown(int index){
+	int rows = (int) (Math.log(size) / Math.log(2));
+	if (2*index+2<size){
+	    if (max && (data[2*index+2].compareTo(data[index])>0 || data[2*index+1].compareTo(data[index])>0)){
+	        if (data[2*index+2].compareTo(data[2*index+1])>0){
+				swap(index, 2*index+2);
+				pushdown(2*index+2);
+			}
+			else{
+				swap(index, 2*index+1);
+				pushdown(2*index+1);
+			}
+	    }
+	    else if(!max && (data[2*index+2].compareTo(data[index])<0 || data[2*index+1].compareTo(data[index])<0)){
+	        if (data[2*index+2].compareTo(data[2*index+1])<0){
+				swap(index, 2*index+2);
+				pushdown(2*index+2);
+			}
+			else{
+				swap(index, 2*index+1);
+				pushdown(2*index+1);
+			}
+	    }
+	}
+	else if (2*index+1<size){
+		if(max && data[2*index+1].compareTo(data[index])>0){
+			swap(index, 2*index+1);
+			pushdown(2*index+1);
+		}
+		else if(!max && data[2*index+1].compareTo(data[index])<0){
+			swap(index,2*index+1);
+			pushdown(2*index+1);
+		}
+	}
+	}
 
     public T peek(){
 		return data[0];
@@ -117,14 +129,18 @@ public class MyHeap<T extends Comparable<T>>{
     }
 	
 	public void heapify(T[] unsorted){
-		for (int n=0; n<unsorted.length; n++){
-			add(unsorted[n]);
+		data = unsorted;
+		size = data.length;
+		for(int n=size-1; n>=0; n--){
+			if(2*n+1<size){
+				pushdown(n);
+			}
 		}
 	}
 
 	@SuppressWarnings("unchecked")
 	public static void main(String[]args){
-		MyHeap heap = new MyHeap<String>(false);
+		/*MyHeap heap = new MyHeap<Integer>(false);
 		heap.add(Integer.valueOf(4));
 		System.out.println(heap);
 		heap.add(Integer.valueOf(8));
@@ -142,6 +158,10 @@ public class MyHeap<T extends Comparable<T>>{
 		heap.add(Integer.valueOf(2));
 		System.out.println(heap);
 		heap.remove();
-		System.out.println(heap);
+		System.out.println(heap);*/
+		MyHeap heap2 = new MyHeap<Integer>(false);
+		Integer[] array = {2,5,3,9,8,7,1,6,5,10};
+		heap2.heapify(array);
+		System.out.println(heap2);
 	}
 }
